1008df9402cee9c5ea817d1b97736c3e6bdd7fb9
John Redden <jtredden@gmail.com>
2017-05-22 09:22:25 -0700

    Init SURGE Graphing Calculator
    A graphing calculator to be proud of.

---
+++ SURGEgrapher/www/scripts/graphpaper.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    console.log("CHA");
+    $("#deviceready").click(function () {
+        //can use this as a splash screen?
+        $(".app").hide();
+    });
+
+
+    // for negatives on the font
+    JXG.Options.ticks.useUnicodeMinus = false;
+    // Initial graph board set-up
+    //JXG.Options.board.renderer = 'canvas';
+
+    board = JXG.JSXGraph.initBoard('box', {
+
+        axis: { ticks: { drawLabels: true }, firstArrow: true, strokeColor: 'black' },
+        grid: { strokeWidth: 0.75 },
+        showCopyright: false,
+        showNavigation: false,
+        keepaspectratio: true,
+        zoom:
+        {
+            enabled: true,
+            factorX: 1.25,
+            factorY: 1.25,
+            wheel: false,
+            needShift: false,
+            min: 0.0001,
+            //max: 10000.0,
+            pinchHorizontal: true,
+            pinchVertical: true,
+            pinchSensitivity: 7
+        },
+        pan:
+        {
+            enabled: true,
+            needTwoFingers: false
+        }
+    });
+    board.resizeContainer($(window).width(), $(window).height());
+
+    board.create('functiongraph', [function (x) { return Math.sin(x); }], { strokeWidth: 2 })
+
+    $(window).on('resize', function () {
+        var bb = board.getBoundingBox();
+        board.resizeContainer($(window).width(), $(window).height(), false, true);//the true = do not call setBoundingBox
+        board.setBoundingBox(bb, false);  //false = keep aspect ratio and same bb as coming in
+    });
+});
