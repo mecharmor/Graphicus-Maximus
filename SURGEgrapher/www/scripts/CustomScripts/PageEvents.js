c5bf25c06e3bfe109df488d29d50ba823a394d4d
Cory Lewis <cjl9703@yahoo.com>
2017-06-14 20:07:18 -0700

    Draggable UI Menu Button
    $("#NavButton").draggable(); wasn't working so I found a script that
    manually allows an html element to be dragged.
    Next Step: on click, radial expand some buttons.

--- SURGEgrapher/www/scripts/CustomScripts/PageEvents.js
+++ SURGEgrapher/www/scripts/CustomScripts/PageEvents.js
@@ -18,6 +18,10 @@
                 $(".app").hide();
             });
 
+            //Draggable Menu Object
+            $('#NavButton').mousedown(handle_mousedown);
+
+
     //Vertical Button Menu
 
         //Home Button Clicked
@@ -74,6 +78,29 @@
                 break;
         }
       
-    }
+        }
+
+    //Allows Any element to be dragged
+    function handle_mousedown(e) {
+        window.my_dragging = {};
+        my_dragging.pageX0 = e.pageX;
+        my_dragging.pageY0 = e.pageY;
+        my_dragging.elem = this;
+        my_dragging.offset0 = $(this).offset();
+        function handle_dragging(e) {
+            var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+            var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+            $(my_dragging.elem)
+                .offset({ top: top, left: left });
+        }
+        function handle_mouseup(e) {
+            $('body')
+                .off('mousemove', handle_dragging)
+                .off('mouseup', handle_mouseup);
+        }
+        $('body')
+            .on('mouseup', handle_mouseup)
+            .on('mousemove', handle_dragging);
+    }//End Mousedown function
 
 });
