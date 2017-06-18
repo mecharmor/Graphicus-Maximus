79168e82e952ccc2402c2125602a521f39c15416
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2017-06-17 19:49:01 -0700

    Hope... Hope is real

--- SURGEgrapher/www/scripts/CustomScripts/PageEvents.js
+++ SURGEgrapher/www/scripts/CustomScripts/PageEvents.js
@@ -1,123 +1,49 @@
 ﻿$(document).ready(function () {
 
     //Global Variables
-        //Algebra Div
-            var algebraDivId = $("#algebraPage");
-        //Trigonometry Div
-            var trigonometryDivId = $("#trigonometryPage");
-        //Calculus Div
-            var calculusDivId = $("#calculusPage");
-        //Graph Div
-            var graphDivId = $("#box");
+    //Algebra Div
+    var algebraDivId = $("#algebraPage");
+    //Trigonometry Div
+    var trigonometryDivId = $("#trigonometryPage");
+    //Calculus Div
+    var calculusDivId = $("#calculusPage");
+    //Graph Div
+    var graphDivId = $("#box");
 
-            //Start at Index View
-            IntView(0);
+    //Start at Index View
+    IntView(0);
 
-            $("#deviceready").click(function () {
-                //can use this as a splash screen?
-                $(".app").hide();
-            });
+    $("#deviceready").click(function () {
+        //can use this as a splash screen?
+        $(".app").hide();
+    });
 
 
     //Button Menu
-            //inner Menu Buttons
-            var btnHome = $("#btnHome");
-            var btnAlgebra = $("#btnAlgebra");
-            var btnTrig = $("#btnTrig");
-            var btnCalc = $("#btnCalc");
-            var btnFunc = $("#btnFunc");
+    //inner Menu Buttons
+    var btnHome = $("#btnHome");
+    var btnAlgebra = $("#btnAlgebra");
+    var btnTrig = $("#btnTrig");
+    var btnCalc = $("#btnCalc");
+    var btnFunc = $("#btnFunc");
 
 
-        //Home Button Clicked
-        btnHome.click(function () {
-            IntView(0);
-        });
-        //Algebra Button Clicked
-        btnAlgebra.click(function () {
-            IntView(1);
-        });
-        //Trig Button Clicked
-       btnTrig.click(function () {
-            IntView(2);
-        });
-        //Calc Button Clicked
-        btnCalc.click(function () {
-            IntView(3);
-        });
-
-
-    //Draggable Menu Set
-    let menu = $("#dragMenu");
-    let _toggle = true;
-    menu.draggable({ containment: "body" });
-    //btnAlgebra.hide();
-
-
-    menu.click(function () {
-
-        if (_toggle) {
-
-            btnCalc
-                .css("top", "3em")
-                .css("left", "3em")
-                .css("opacity", "1")
-                .css("transform", "rotate(360deg)");
-
-            btnAlgebra
-                .css("top", "3em")
-                .css("opacity", "1")
-                .css("transform", "rotate(360deg)");
-
-            btnFunc
-                .css("top", "-3em")
-                .css("opacity", "1")
-                .css("transform", "rotate(360deg)");
-
-            btnHome
-                .css("left", "3em")
-                .css("opacity", "1")
-                .css("transform", "rotate(360deg)");
-
-            btnTrig
-                .css("left", "-3em")
-                .css("opacity", "1")
-                .css("transform", "rotate(360deg)");
-           
-            _toggle = false;
-
-        } else {
-
-            btnCalc
-                .css("top", "0em")
-                .css("left", "0em")
-                .css("opacity", "0")
-                .css("transform", "rotate(0deg)");
-
-            btnAlgebra
-                .css("top", "0em")
-                .css("opacity", "0")
-                .css("transform", "rotate(0deg)");
-
-            btnFunc
-                .css("top", "0em")
-                .css("opacity", "0")
-                .css("transform", "rotate(0deg)");
-
-            btnHome
-                .css("left", "0em")
-                .css("opacity", "0")
-                .css("transform", "rotate(0deg)");
-
-            btnTrig
-                .css("left", "0em")
-                .css("opacity", "0")
-                .css("transform", "rotate(0deg)");
-
-            _toggle = true;
-        }
-
+    //Home Button Clicked
+    btnHome.click(function () {
+        IntView(0);
     });
-
+    //Algebra Button Clicked
+    btnAlgebra.click(function () {
+        IntView(1);
+    });
+    //Trig Button Clicked
+    btnTrig.click(function () {
+        IntView(2);
+    });
+    //Calc Button Clicked
+    btnCalc.click(function () {
+        IntView(3);
+    });
 
     //index = 0, algebra = 1, trigonometry = 2, calculus = 3, etc...
     function IntView(x) {
@@ -154,62 +80,56 @@
                 graphDivId.hide();
                 break;
         }
+    }
 
     //Draggable Menu Set
     let menu = $("#dragMenu");
-    let menuItem1 = $("#menuItem1");
-    let menuItem2 = $("#menuItem2");
-    let menuItem3 = $("#menuItem3");
-    let menuItem4 = $("#menuItem4");
-    let menuItem5 = $("#menuItem5");
-    let menuItem6 = $("#menuItem6");
-    let menuItem7 = $("#menuItem7");
-    let menuItem8 = $("#menuItem8");
-    let menuItems = [
-        menuItem1,
-        menuItem2,
-        menuItem3,
-        menuItem4,
-        menuItem5,
-        menuItem6,
-        menuItem7,
-        menuItem8
-    ];
+    let menuItems = [];
     let _toggle = true;
-    menu.draggable({ containment: "body" });
+
+    for (let i = 1; i <= 6; i++) {
+        menuItems[q-1]= $("#menuItem" + i);
+    }
+    menu.draggable({ containment: "#box" });
     //menuItem1.hide();
 
-});//document.ready
-
+    menu.click(function () {
         // Shows the menu
         if (_toggle) {
             let top = 0;
             let left = 0;
+
             for (let i = 0; i < menuItems.length; i++) {
                 switch (i) {
-                    case 0: // Alg button
-                        top = -5;
-                        break;
-                    case 1: // Trig button
+                    case 0: // f(x) button
+                        // Placed north west
                         top = -2.5;
                         left = -4;
                         break;
-                    case 2: // Calc button
+                    case 1: // home button
+                        // Placed north
+                        top = -5;
+                        break;
+                    case 2: // Settings button
+                        // Placed north east
                         top = -2.5;
                         left = 4;
                         break;
                     case 3: // Calc button
+                        // Placed south east
                         top = 2.5;
                         left = 4;
                         break;
-                    case 4: // Calc button
+                    case 4: // Trig button
+                        // Placed south
                         top = 5;
                         break;
-                    case 5: // Calc button
+                    case 5: // Alg button
+                        // Placed south west
                         top = 2.5;
                         left = -4;
                         break;
-                }
+                } // End of switch
                 menuItems[i]
                     .css("top", top + "em")
                     .css("left", left+"em")
@@ -217,7 +137,7 @@
                     .css("opacity", "1");
                 top = 0;
                 left = 0;
-            }
+            } // End of for loop
             _toggle = false;
 
         } else {
@@ -234,16 +154,10 @@
 
     });
 
-    menuItem1.click(function () {
-
-        //Load Algebra Page
-        IntView(1);
-
-    });
-
 
 });//document.ready
-//Function Button Clicked  
+
+//Function Button Clicked
 function boxBlur(x) {
     $("#box").css("filter", "blur(" + x + "px)");
 };
