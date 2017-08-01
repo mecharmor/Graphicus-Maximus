3d11dd66535ddb4288bb65ad0c108d8577587127
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2017-08-01 16:32:45 -0700

    Major Fixes to the LaTeX Fix
    x^2^2 and -x^-2 works

--- SURGEgrapher/www/scripts/CustomScripts/globalFunctions.js
+++ SURGEgrapher/www/scripts/CustomScripts/globalFunctions.js
@@ -32,15 +32,16 @@ $().ready(function () {
     }, 1000);
 });
 
+// Converts from text to LaTeX
 function txt2tex(txt) {
     // Variables
     const tree = createExpressionTree(txt.replace(/\s/g, ""));
     const latex = constructTree(tree);
     
     return latex;
-    //return txt.replace(/(sin|cos|tan|csc|sec|cot|log|sinh|cosh|tanh|csch|sech|coth)/g, "\\$1 ").replace("(", "{").replace(")", "}");
 }
 
+// Constructs the binary expression tree
 function constructTree(tree) {
     if (typeof (tree) != "object")
         return tree;
@@ -48,36 +49,32 @@ function constructTree(tree) {
     // Variables
     let str = "";
 
+    // This is usually the root
     if (typeof (tree[0]) == "object") {
         str = constructTree(tree[0]);
         return str;
     }
     switch (tree[0]) {
         case '+': {
-            str = constructTree(tree[1]) + "+" + constructTree(tree[2]);
+            str = constructTree(tree[1]) + " + " + constructTree(tree[2]);
         } break;
         case '-': {
-            str = constructTree(tree[1]) + "-" + constructTree(tree[2]);
+            str = constructTree(tree[1]) + " - " + constructTree(tree[2]);
         } break;
         case '*': {
-            str = constructTree(tree[1]) + "\\cdot" + constructTree(tree[2]);
+            str = constructTree(tree[1]) + " \\cdot " + constructTree(tree[2]);
         } break;
         case '/': {
-            str= "\\dfrac{" + constructTree(tree[1]) + "}{" + constructTree(tree[2]) + "}";
+            str= " \\dfrac{" + constructTree(tree[1]) + "}{" + constructTree(tree[2]) + "} ";
         } break;
         case '^': {
-            str = constructTree(tree[1]) + "^{" + constructTree(tree[2]) + "}";
+            str = constructTree(tree[1]) + "^{" + constructTree(tree[2]) + "} ";
         } break;
         case '{': {
-            str = "\\" + tree[1] + "{(" + constructTree(tree[2]) + ")}";
+            str = " \\" + tree[1] + "{(" + constructTree(tree[2]) + ")} ";
         } break;
-        case '(': {
-            if (tree[1] == undefined || tree[1]== null || tree[1]== "undefined") {
-                str = "(" + constructTree(tree[2]) + ")";
-            }
-            else {
-                str = constructTree(tree[1]) + "\\cdot(" + constructTree(tree[2]) + ")";
-            }
+        case "()": {
+            str = " (" + constructTree(tree[1]) + ") ";
         } break;
         default: {
             str = tree[0];
@@ -87,38 +84,70 @@ function constructTree(tree) {
     return str;
 }
 
-function displayTree(tree) {
+// Displays the tree, this is for debug reasons only
+function displayTree(tree, index) {
     // Variables
     let str = "[";
+
+    if (!index)
+        index = 0;
+
+    switch (index % 3) {
+        case 0: str = ' [ '; break;
+        case 1: str = ' { '; break;
+        case 2: str = ' ( '; break;
+    }
     
     for (let i = 0; i < tree.length; i++) {
         if (typeof (tree[i]) == "object")
-            str += displayTree(tree[i]);
+            str += displayTree(tree[i], index + 1);
         else
             str += tree[i];
         if (i < tree.length - 1) {
             str += ", ";
         }
     }
+    switch (index % 3) {
+        case 0: str += ' ] '; break;
+        case 1: str += ' } '; break;
+        case 2: str += ' ) '; break;
+    }
 
-    return str + "]";
+    return str;
+}
+
+function isSpecialChar(c) {
+    switch (c) {
+        case '(':
+        case '+':
+        case '*':
+        case '/':
+        case '^':
+        case '{':
+            return true;
+    }
+
+    return false;
 }
 
 function getPrecedence(op) {
     switch (op) {
-        default:
-            return 0;
         case '+':
         case '-':
-            return 2;
+            return 1;
         case '/':
         case '*':
-            return 3;
+            return 2;
         case '^':
+            return 3;
+        case '{':
             return 4;
+        default:
+            return 0;
     }
 }
 
+// Replaces the given index in the string. Basically (str[index]= replacement)
 function replaceAt(str, index, replacement) {
     return str.substr(0, index) + replacement + str.substr(index + replacement.length);
 }
@@ -127,20 +156,63 @@ function createExpressionTree(txt) {
     // Variables
     let tree = [];
     let operatorStack = [];
-    let funcStack = [];
-    
+    let txtIndex = -1;
+
     function doBinary(op) {
+        // If it is a parenthesis, then only have it as ["()", operand]
+        if (op == "()") {
+            // Variables
+            let all = tree.pop();
+
+            tree.push([op, all]);
+            return;
+        }
         // Variables
         let right = tree.pop();
         let left = tree.pop();
-
+        
         tree.push([op, left, right]);
     }
 
+    // Makes sure that )( multiplies, but not )- kind of thing
+    do {
+        txtIndex = txt.indexOf(')', txtIndex + 1);
+        if (txtIndex>= 0 && txtIndex != txt.length - 1) {
+            switch (txt[txtIndex + 1]) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                case ')':
+                    break;
+                default:
+                    txt = (txt.substring(0, txtIndex + 1) + "*" + txt.substring(txtIndex + 1));
+                    break;
+            }
+        }
+    } while (txtIndex != -1 && txtIndex< txt.length);
+
     for (let i = 0; i < txt.length; i++) {
         switch (txt[i]) {
+            case '(':
+            case '{':
+                operatorStack.push(txt[i]);
+                break;
+            case ')':
+                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] != '(') {
+                    doBinary.call(this, operatorStack.pop());
+                }
+                //operatorStack.push(txt[i]);
+                operatorStack.pop();
+                doBinary.call(this, "()");
+                break;
+            case '}':
+                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] != '{') {
+                    doBinary.call(this, operatorStack.pop());
+                }
+                break;
             case '+':
-            case '-':
             case '*':
             case '/':
                 // Variables
@@ -152,23 +224,27 @@ function createExpressionTree(txt) {
                 operatorStack.push(txt[i]);
 
                 break;
-            case '(':
-            case '{':
-                operatorStack.push(txt[i]);
-                break;
-            case ')':
-                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] != '(') {
-                    doBinary.call(this, operatorStack.pop());
+            case '-':
+                if (i > 0 && !isSpecialChar(txt[i - 1])) {
+                    // Variables
+                    let prec = getPrecedence(txt[i]);
+
+                    while (operatorStack.length > 0 && prec <= getPrecedence(operatorStack[operatorStack.length - 1])) {
+                        doBinary.call(this, operatorStack.pop());
+                    }
+                    operatorStack.push(txt[i]);
+
+                    break;
                 }
-                operatorStack.pop();
-                break;
-            case '}':
-                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] != '(') {
-                    doBinary.call(this, operatorStack.pop());
-                }
-                break;
             default:
+                // If the command is ^ with no enclosure then make sure you can do x^2^2 kind of stuff
                 if (txt[i] == '^' && txt[i] != '(') {
+                    // If there was an already registered ^ command, then push it to the stack without taking anything out
+                    if (operatorStack[operatorStack.length - 1] == '^') {
+                        operatorStack.push(txt[i]);
+                        break;
+                    }
+                    // Else do everything as if it's normal
                     // Variables
                     let prec = getPrecedence(txt[i]);
 
@@ -184,15 +260,21 @@ function createExpressionTree(txt) {
                 let parenth = false;
 
                 while (i < txt.length) {
+                    // Continues until a command ( +-*/ kind of stuff )
                     if (!txt[i].match(/\w/)) {
                         if (txt[i] == '(')
                             parenth = true;
-                        break;
+                        if (txt[i] == '-' && i == n) {
+                            // Do nothing for effect
+                        }
+                        else
+                            break;
                     }
                     i++;
                 }
                 // Variables
                 let str = txt.substring(n, i--);
+                let q = -1;
 
                 // If there was a parenthesis at the end of this operand, then
                 // find if it is a function of some kind
@@ -216,6 +298,8 @@ function createExpressionTree(txt) {
                                     c--;
                                 }
                             }
+                            q = a;
+                            break;
                         }
                     }
                 }
@@ -223,7 +307,23 @@ function createExpressionTree(txt) {
                     str = str.replace(/(sin|cos|tan|csc|sec|cot|sinh|cosh|tanh|csch|sech|coth|log|ln)/g, "\\$1 ");
                 }
 
-                tree.push(str);
+                // If there is no command slapped in, then push regularly
+                if(q<= 0)
+                    tree.push(str);
+                // Else push the first bit, push a multiplier, lastly push the command
+                else {
+                    // Variables
+                    let prec = getPrecedence('*');
+
+                    tree.push(str.substring(0, q));
+
+                    while (operatorStack.length > 0 && prec <= getPrecedence(operatorStack[operatorStack.length - 1])) {
+                        doBinary.call(this, operatorStack.pop());
+                    }
+                    operatorStack.push('*');
+
+                    tree.push(str.substring(q));
+                }
                 break;
         }
     }
